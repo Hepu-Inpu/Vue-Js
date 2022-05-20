233ffcdbbe8ee90492891e7e0fb85ebc2dfcd2a4
Hepu-Inpu <juammarin@misena.edu.co>
2022-05-19 19:03:46 -0500

    realizamos reactivo el input con buscar

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 const API = "https://api.github.com/users/";
 
-async function buscar() {
-    const response = await fetch(API + "Hepu-Inpu")
-    //traemos la informacion en formato JSON
-    const data = await response.json();
-    console.log(data)
-}
-//se crea un modelo
+
+//se crea una instancia de vue 
 const app = Vue.createApp({
     data() {
         return {
-            message: 'cambio afirmado'
+            message: 'cambio afirmado',
+            busqueda: null,
+            result: null,
+        }
+    },
+    //la palabra function ya no es necesaria ya que se usa methods
+    methods: {
+        async buscar() {
+            const response = await fetch(API + this.busqueda)
+            //traemos la informacion en formato JSON
+            const data = await response.json();
+            console.log(data)
+            this.result = true
         }
     }
 })//montamos esta informacion en el div de html
